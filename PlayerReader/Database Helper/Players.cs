a26144e28e856d70083718702dd9eb85c744b70d
Larson <mlarson@osii.com>
2019-11-10 18:48:23 -0600

    commiting player reader work to repo

---
+++ PlayerReader/Database Helper/Players.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayerReader
+{
+    class Players
+    {
+        public string PlayerName { get; private set; }
+
+        public uint PlayerNumber { get; private set; }
+        public string PlayerPosition { get; private set; }
+        public string PlayerSport { get; private set; }
+
+        public Players(string playerName, uint playerNumber, string playerPosition, string playerSport)
+        {
+            this.PlayerName = playerName;
+            this.PlayerNumber = playerNumber;
+            this.PlayerPosition = playerPosition;
+            this.PlayerSport = playerSport;
+        }
+
+        public override string ToString()
+        {
+            string retString = PlayerName + " is number " + PlayerNumber.ToString() + " and plays " + PlayerSport + " at position " + PlayerPosition;
+            return retString;
+        }
+
+    }
+}
---
+++ PlayerReader/Database Helper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+using System.Reflection;
+
+namespace PlayerReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string path = Path.Combine(directory, "input/PlayerData.csv");
+            IEnumerable<string> playerFile = File.ReadAllLines(path).Skip(1);
+            List<Players> players = new List<Players>();
+
+            foreach (string playerLine in playerFile)
+            {
+                string name = playerLine.ToString().Split(',')[0];
+                uint number = Convert.ToUInt16(playerLine.ToString().Split(',')[1]);
+                string position = playerLine.ToString().Split(',')[2];
+                string sport = playerLine.ToString().Split(',')[3];
+
+                players.Add(new Players (name, number, position, sport));
+            }
+
+            Console.WriteLine("These are the players on file:");
+            foreach (Players player in players)
+            {
+                Console.WriteLine(player.ToString());
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
